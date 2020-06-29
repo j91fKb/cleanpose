1619d9eae09eca3c15b1250ee165ecfa3bfae49d
kuchi <azhang4@partners.org>
2020-06-29 03:11:24 -0400

    updated utils

--- src/cleanpose/utils.py
+++ src/cleanpose/utils.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 
 
-def get_folders_with_pattern(folder, pattern=''):
+def get_folders_with_pattern(folder, patterns=[]):
     folder = Path(folder)
 
     folders = set()
 
-    for file in folder.rglob(f"**/*{pattern}*"):
+    for file in folder.rglob('*'):
+        if file.is_file() and (file.parent in folders or not matches(str(file), patterns)):
+            continue
         folders.add(file.parent)
 
     return folders
+
+
+def matches(string, patterns):
+    for pattern in patterns:
+        if pattern not in string:
+            return False
+    return True
